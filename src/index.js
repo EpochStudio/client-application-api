0223d2a67d0f00e13eb955dbeb9aff25352c672c
chrissch.dev <66461609+ChrisSch-dev@users.noreply.github.com>
2025-05-07 19:53:53 +0800

    feat: changes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const express = require('express')
 const ratelimit = require('express-rate-limit')
 const fetch = require('node-fetch')
 const config = require('./config.js')
-const { category } = require('./assets/info.json')
-const { formatters } = require('./utils')
+const { category, commands } = require('./assets/info.json')
+const { sorting } = require('./utils')
 
 const app = express()
 const limiter = ratelimit(config.ratelimitConfig)
@@ -13,29 +13,41 @@ const limiter = ratelimit(config.ratelimitConfig)
 app.use(limiter)
 
 app.get('/', (req, res) => {
-    res.redirect('/gimmie/commands')
+    res.redirect('/gimmie/commands/all')
 })
 
-/**
- * @param req
- * @param res
- * @returns {Array<categories}
- */
 app.get('/list', (req, res) => {
     res.status(200).json({
         categories: category
     })
 })
 
-/**
- * @param req
- * @param res
- * @returns {Object}
- */
-app.get('/gimmie/commands/', async (req, res) => {
+app.get('/version', (req, res) => {
+  res.status(200).json({
+      api: config.version,
+      client: "5.1.0"
+  })
+})
+
+app.get('/gimmie/commands/:cate', async (req, res) => {
     if (!process.env.BOT_ID || !process.env.BOT_TOKEN) {
-        res.status(200).json({})
+        return res.status(500).json({
+            error: "Error 500 - Internal Server Error!",
+            type: "Not Configured"
+        })
     }
+
+    /**
+     * @type {string}
+     */
+    const searchParams = req.params.cate;
+
+    if (!category.includes(searchParams)) {
+        return res.status(400).json({
+            error: "Error 400 - Bad Request!"
+        })
+    }
+
 
     try {
         const fetchApi = await fetch(`https://discord.com/api/v10/applications/${process.env.BOT_ID}/commands`, {
@@ -46,17 +58,23 @@ app.get('/gimmie/commands/', async (req, res) => {
 
         if (fetchApi.ok) {
             const data = await fetchApi.json()
+            const sort = sorting(data, commands, searchParams)
 
             res.status(200).json({
-                commands: !data ? [] : data
+                length: sort.toHuman.length,
+                fetchedCategory: searchParams,
+                commands: sort.toHuman,
+                raw: sort.raw
             })
         } else {
-            res.status(200).json({ status: 0 })
+            return res.status(404).json({ error: "Error 404 - Not Found!" })
         }
     } catch (err) {
         console.log(err)
 
-        res.status(400).json({
+        res.status(500).json({
+            error: "Error 500 - Internal Server Error!",
+            type: "Runtime Error",
             message: err.message,
             fullErr: err
         })
